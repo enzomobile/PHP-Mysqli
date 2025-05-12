3fbeae264df1ebba3847ded52d15ae852f998076
Enzo Móbile de Oliveira <prenzo0001@gmail.com>
2025-05-12 16:42:33 -0300

    Criação de index e cadastro, atualização necessária

--- Projeto/index.php
+++ Projeto/index.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CRUD - Teste</title>
+    <title>Bem vindo</title>
 </head>
 <body>
-    <?php
     
-    ?>
 </body>
 </html>
